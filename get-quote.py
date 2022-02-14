89277165a11fb073632e188e8f3a38d999441f21
adeb08 <debnbcc@outlook.com>
2022-02-14 15:41:41 -0400

    Dynamic input and output

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,31 @@
-import random
+#import random
 def first():
  # print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  #last = 13
+  #rnd = random.randint(0, last)
+  #for i in range(len(quotes)):
+  count = 0
+  for line in quotes:
+    count += 1
+    print(line)
+  print("Please enter the line numbers from 0-", count,":")
+  num = int(input())
+  print(quotes[num])
+ 
+ #Write New Line
+  f = open("quotes.txt", "a")
+  line = input("Enter new line:")
+  f.write("\n")
+  f.write(line)
+  f.close()
+  #f = open("quotes.txt")
+  lst = list(open("quotes.txt"))
+  lastline = lst[-1]  
+  print("This is the latest line:", lastline)
 
 if __name__== "__main__":
   first()
